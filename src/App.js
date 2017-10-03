67cecb8b85eba0ede8add1602735d63592c6a61b
Aaron Shafovaloff <aaronshaf@gmail.com>
2017-10-03 09:45:18 -0600

    rename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>react-block-game</h1>
+        <h1>react-barrier-game</h1>
         <Game boardSize={12} />
       </div>
     )
